2bb4d0279951657499894b23c5b5cd006535a438
Shivam singh <shivamsingh8496@gmail.com>
2022-10-21 21:21:12 +0530

    Project completed

--- BloggingSite/BloggingApp/views.py
+++ BloggingSite/BloggingApp/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render,redirect
 
 from .models import post
 
-
-
+# Create your views here.
 def index(request):
     p1 = post.objects.all()
     return render(request,'index.html',{"posts":p1})
 
-# Create your views here.
+def posts(request,pk):
+    p1 =post.objects.get(id=pk)
+    return render(request,'posts.html',{'pk':p1})
+    
