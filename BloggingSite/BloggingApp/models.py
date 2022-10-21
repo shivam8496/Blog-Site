6701be15144361cd7ec7a1244878c345e0f82ac1
Shivam singh <shivamsingh8496@gmail.com>
2022-10-21 19:23:58 +0530

    Created database named post

--- BloggingSite/BloggingApp/models.py
+++ BloggingSite/BloggingApp/models.py
@@ -1,3 +1,10 @@
+from email.policy import default
+from operator import truediv
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class post (models.Model):
+    title = models.CharField(max_length=100)
+    body =   models.CharField(max_length=100000)
+    createdAt = models.DateTimeField(default = datetime.now,blank=True)
